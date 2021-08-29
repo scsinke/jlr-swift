1909b659e4bb93655aece550aebd01d89e04447c
Stein Carsten Sinke <carsten@q42.nl>
2021-08-29 18:52:58 +0200

    Add lostPassword, fetchUserById, fetchVehicles, updateUserProfile

--- Sources/JLR-Swift/UserService/UserService.swift
+++ Sources/JLR-Swift/UserService/UserService.swift
@@ -4,11 +4,85 @@ import Combine
 protocol JLRUserService {
     func authenticate(username: String, password: String, deviceID: String) async throws -> Authentication
     func authenticate(refreshToken: String, deviceID: String) async throws -> Authentication
-    func fetchUser(accessToken: String, loginName: String, deviceID: String) async throws -> User
-    func registerDevice(accessToken: String, username: String, authorizationToken: String, expiresIn: Int, deviceID: String) async throws -> Void
+    func fetchUserByName(accessToken: String, loginName: String, deviceID: String) async throws -> User
+    func registerDevice(accessToken: String, username: String, authorizationToken: String, expiresIn: Int, deviceID: String) async throws
+    func recoverPassword(userId: String, deviceID: String) async throws
+    func fetchUserById(accessToken: String, deviceID: String, userID: String) async throws -> User
+    func fetchVehiclesFromUser(accessToken: String, deviceID: String, userID: String) async throws -> [Vehicle]
+    func updateUserInfo(accessToken: String, deviceID: String, userID: String) async throws
 }
 
 public struct UserService: JLRUserService {
+    func fetchUserById(accessToken: String, deviceID: String, userID: String) async throws -> User {
+        guard let url = URL(string: "\(URLHost.IF9)/users/\(userID)") else {
+            throw APIError.invalidEndpoint
+        }
+        
+        var req = URLRequest(url: url)
+        
+        req.allHTTPHeaderFields = [
+            "Accept": "application/vnd.wirelesscar.ngtp.if9.User-v3+json",
+            "Content-Type": "application/json",
+            "Authorization":"Bearer \(accessToken)",
+            "X-Device-Id": deviceID,
+        ]
+        
+        let (data, _) = try await URLSession.shared.data(for: req)
+        
+        return try JSONDecoder().decode(User.self, from: data)
+    }
+    
+    func fetchVehiclesFromUser(accessToken: String, deviceID: String, userID: String) async throws -> [Vehicle] {
+        guard let url = URL(string: "\(URLHost.IF9)/users/\(userID)/vehicles?primaryOnly=true") else {
+            throw APIError.invalidEndpoint
+        }
+        
+        var req = URLRequest(url: url)
+        
+        req.allHTTPHeaderFields = [
+            "Content-Type": "application/json",
+            "Authorization":"Bearer \(accessToken)",
+            "X-Device-Id": deviceID,
+        ]
+        
+        let (data, _) = try await URLSession.shared.data(for: req)
+        
+        return try JSONDecoder().decode([Vehicle].self, from: data)
+    }
+    
+    func updateUserInfo(accessToken: String, deviceID: String, userID: String) async throws {
+        guard let url = URL(string: "\(URLHost.IF9)/users/\(userID)") else {
+            throw APIError.invalidEndpoint
+        }
+        
+        var req = URLRequest(url: url)
+        
+        req.allHTTPHeaderFields = [
+            "Content-Type": "application/vnd.wirelesscar.ngtp.if9.User-v3+json; charset=utf-8",
+            "Authorization":"Bearer \(accessToken)",
+            "X-Device-Id": deviceID,
+        ]
+        
+        let (_, _) = try await URLSession.shared.data(for: req)
+    }
+    
+    func recoverPassword(userId: String, deviceID: String) async throws {
+        guard let url = URL(string: "\(URLHost.IF9)/users/\(userId)") else {
+            throw APIError.invalidEndpoint
+        }
+        
+        var req = URLRequest(url: url)
+        
+        req.allHTTPHeaderFields = [
+            "Accept": "application/vnd.wirelesscar.ngtp.if9.User-v3+json",
+            "Content-Type": "application/json",
+            "Authorization":"Basic aGlxOnNvbW1hcjEy",
+            "X-Device-Id": deviceID,
+        ]
+        
+        let (_, _) = try await URLSession.shared.data(for: req)
+    }
+    
     func authenticate(refreshToken: String, deviceID: String) async throws -> Authentication {
         let body = RefreshTokenBody(refreshToken: refreshToken)
         return try await makeAuthRequest(body: body, deviceId: deviceID)
@@ -19,7 +93,7 @@ public struct UserService: JLRUserService {
         return try await makeAuthRequest(body: body, deviceId: deviceID)
     }
     
-    func fetchUser(accessToken: String, loginName: String, deviceID: String) async throws -> User {
+    func fetchUserByName(accessToken: String, loginName: String, deviceID: String) async throws -> User {
         guard let url = URL(string: "\(URLHost.IF9)/users?loginName=\(loginName)}") else {
             throw APIError.invalidEndpoint
         }
